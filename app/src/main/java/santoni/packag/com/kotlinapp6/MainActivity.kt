86e7a468c37ceec6ea72c7e89125210e8758b1e6
sungzz <32294250+sungzz@users.noreply.github.com>
2018-09-26 18:00:00 +0800

    update

--- app/src/main/java/santoni/packag/com/kotlinapp6/MainActivity.kt
+++ app/src/main/java/santoni/packag/com/kotlinapp6/MainActivity.kt
@@ -2,11 +2,15 @@ package santoni.packag.com.kotlinapp6
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var sportAdapter = SportAdapter(this@MainActivity)
+        gridView.adapter = sportAdapter
     }
 }
--- app/src/main/java/santoni/packag/com/kotlinapp6/SportDatabase.kt
+++ app/src/main/java/santoni/packag/com/kotlinapp6/SportDatabase.kt
@@ -6,5 +6,28 @@ class SportDatabase {
 
     constructor() {
 
+        sports = ArrayList()
+
+        sports?.add(Sport("Boxing", "Boxing is a combat sport in which two people," +
+                " usually wearing protective gloves," +
+                " throw punches at each other for a predetermined set of time" +
+                " in a boxing ring", R.drawable.boxing))
+        sports?.add(Sport("Wrestling", "Wrestling is a combat sport involving grappling type" +
+                " technigues such as clinch fighting, throws and takedowns, joint locks," +
+                " pins and other grappling holds.", R.drawable.wrestling))
+        sports?.add(Sport("Taekwondo", "Taekwondo is a korean martial art" +
+                " characterized by its emphasis on head-height kicks," +
+                " jumping and spinning kicks, and fast kicking techniques.", R.drawable.taekwondo))
+        sports?.add(Sport("Crossfit", "Crossfit is a branded fitness regimen created" +
+                " by Greg Glassman and is a registered trademark of CrossFit, Inc.", R.drawable.crossfit))
+        sports?.add(Sport("Karate", "Karate is a martial art developed in" +
+                " the Ryukyu Kingdom,", R.drawable.karate))
+        sports?.add(Sport("Football", "Associaltion football, more commonly known as" +
+                " football or soccer, is a team sport played between" +
+                " two teams of eleven players with a spherical ball", R.drawable.football))
+        sports?.add(Sport("Judo", "udo was created as" +
+                " a physical, mental and moral pedagogy in Japan," +
+                " in 1882, by Jigoro Kano", R.drawable.jiujitsu))
+
     }
 }
